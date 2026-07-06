d43755bfb81c7083d1c8276e053e105067695f3a
NateTheMushroom <nate09tgd@gmail.com>
2026-07-05 23:05:30 -0400

    yuh!

---
+++ src/main/java/net/mshm/jeffbs/JeffSBull.java
@@ -0,0 +1,22 @@
+package net.mshm.jeffbs;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JeffSBull implements ModInitializer {
+	public static final String MOD_ID = "jeffbs";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.fromNamespaceAndPath(MOD_ID, path);
+	}
+}
---
+++ src/main/java/net/mshm/jeffbs/JeffSBullDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mshm.jeffbs;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class JeffSBullDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
